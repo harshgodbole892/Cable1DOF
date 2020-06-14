cac7e140aa82ca39abd1f0df899d7cde1f8c054d
harshgodbole892 <harsh.godbole@mail.mcgill.ca>
2020-06-14 13:16:24 -0400

    - Configured animation script

--- src/pysrc/L01Analysis.py
+++ src/pysrc/L01Analysis.py
@@ -11,14 +11,42 @@ os.chdir("../../")
 
 from src.pysrc.L01CppRunner import L01CppRunner as CppR
 from src.pysrc.L01LoadData  import L01DataArray as DatArr
+from src.pysrc.L01Animate   import L01Animate
 
-# Created an instance of CPP runner to build CPP solution: 
+Build   = 1
+Run     = 1
+Load    = 1
+Plot    = 1
+Animate = 1
+
+# Create Runner Obj
 runner = CppR()
-runner.Build()
-runner.RunTest()
+    
+# Created an instance of CPP runner to build CPP solution: 
+if (Build == 1) : 
+    print("C++ : Build Started")
+    runner.Build()
+    print("C++ : Build Complete")
+
+if (Run == 1) :     
+    print("C++ : Executing Test Started")
+    runner.RunTest()
+    print("C++ : Data generation complete")
 
 # Plot script here: 
+if (Load == 1):
+    print("Python: Loading results")
+    op = DatArr()
+    print("Python: Loading complete!")
 
-op = DatArr()
-#op.PlotAll()
-op.PlotCustom()
+    if (Plot == 1) :
+        print("Python: Plotting results")
+        op.PlotCustom()
+        #op.PlotAll()
+        print("Python: Plotting complete!")
+
+    if (Animate == 1): 
+        # Animation script
+        print("Python: Generating animation gif")
+        L01Animate.AnimateCable1D(op)
+        print("Python: Animation Ready!")
